a82da64c8455b710483dbedfddda33a23443cd6c
enter-the-code <ssoonngg53@naver.com>
2022-05-11 13:56:19 +0900

    치즈문제 solve

---
+++ PS/PS/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  PS
+//
+//  Created by 송기영 on 2022/05/11.
+//
+#include <queue>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+    return 0;
+}
